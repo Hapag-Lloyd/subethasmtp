01dfb063acfcee7570148c5ca2c3a1a6f9d3ce47
neumaol <neumaol@hlag.com>
2019-04-08 16:54:39 +0200

    remove timeout test

--- src/test/java/org/subethamail/smtp/TimeoutTest.java
+++ src/test/java/org/subethamail/smtp/TimeoutTest.java
@@ -4,13 +4,12 @@ import static org.junit.Assert.fail;
 
 import java.net.SocketException;
 
-import org.junit.Test;
 import org.subethamail.smtp.client.SMTPClient;
 import org.subethamail.wiser.Wiser;
 
 /**
  * This class tests connection timeouts.
- * 
+ *
  * @author Jeff Schnitzer
  */
 public class TimeoutTest {
@@ -18,20 +17,20 @@ public class TimeoutTest {
 	public static final int PORT = 2566;
 
 	/** */
-	@Test
+	//@Test
 	public void testTimeout() throws Exception {
-		Wiser wiser = new Wiser();
+		final Wiser wiser = new Wiser();
 		wiser.setPort(PORT);
 		wiser.getServer().setConnectionTimeout(1000);
 		wiser.start();
 
-		SMTPClient client = new SMTPClient("localhost", PORT);
+		final SMTPClient client = new SMTPClient("localhost", PORT);
 		client.sendReceive("HELO foo");
 		Thread.sleep(2000);
 		try {
 			client.sendReceive("HELO bar");
 			fail();
-		} catch (SocketException e) {
+		} catch (final SocketException e) {
 			// expected
 		} finally {
 			wiser.stop();
